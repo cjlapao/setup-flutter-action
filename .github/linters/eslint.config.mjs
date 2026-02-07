1894fa1d7aa6b458aaac00ee636b390c18da20bd
Carlos Lapao <cjlapao@gmail.com>
2026-02-07 12:18:36 +0000

    fix prettier failure

--- .github/linters/eslint.config.mjs
+++ .github/linters/eslint.config.mjs
@@ -1,82 +1,83 @@
-import jest from "eslint-plugin-jest";
+import jest from 'eslint-plugin-jest'
 import prettier from 'eslint-plugin-prettier'
 import typescript from '@typescript-eslint/eslint-plugin'
-import globals from "globals";
-import babelParser from "@babel/eslint-parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from 'globals'
+import babelParser from '@babel/eslint-parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+})
 
-export default [{
+export default [
+  {
     ignores: [
-        '!**/.*',
-        '**/node_modules/.*',
-        '**/dist/.*',
-        '**/coverage/.*',
-        '**/__tests__/.*',
-        '**/*.json',
-        '__tests__/.*',
-        '__tests__/*',
-        'lib/.*',
-        'coverage/*',
-        'dist/*',
-        'lib/*',
-        '.github/*',
-        'eslint.config.mjs'
-    ],
-}, ...compat.extends(
-    "eslint:recommended",
-    "plugin:jest/recommended",
-), {
+      '!**/.*',
+      '**/node_modules/.*',
+      '**/dist/.*',
+      '**/coverage/.*',
+      '**/__tests__/.*',
+      '**/*.json',
+      '__tests__/.*',
+      '__tests__/*',
+      'lib/.*',
+      'coverage/*',
+      'dist/*',
+      'lib/*',
+      '.github/*',
+      'eslint.config.mjs'
+    ]
+  },
+  ...compat.extends('eslint:recommended', 'plugin:jest/recommended'),
+  {
     plugins: {
-        jest,
-        prettier,
-        typescript
+      jest,
+      prettier,
+      typescript
     },
 
     languageOptions: {
-        globals: {
-            ...globals.commonjs,
-            ...globals.jest,
-            ...globals.node,
-            Atomics: "readonly",
-            SharedArrayBuffer: "readonly",
-        },
+      globals: {
+        ...globals.commonjs,
+        ...globals.jest,
+        ...globals.node,
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+      },
 
-        parser: babelParser,
-        ecmaVersion: 2023,
-        sourceType: "module",
+      parser: babelParser,
+      ecmaVersion: 2023,
+      sourceType: 'module',
 
-        parserOptions: {
-            requireConfigFile: false,
+      parserOptions: {
+        requireConfigFile: false,
 
-            babelOptions: {
-                babelrc: false,
-                configFile: false,
-                presets: ["jest"],
-            },
-        },
+        babelOptions: {
+          babelrc: false,
+          configFile: false,
+          presets: ['jest']
+        }
+      }
     },
 
     rules: {
-        camelcase: "off",
-        "eslint-comments/no-use": "off",
-        "eslint-comments/no-unused-disable": "off",
-        "i18n-text/no-en": "off",
-        "import/no-commonjs": "off",
-        "import/no-namespace": "off",
-        "no-console": "off",
-        "no-unused-vars": "off",
-        "prettier/prettier": "error",
-        semi: "off",
-    },
-}];
+      camelcase: 'off',
+      'eslint-comments/no-use': 'off',
+      'eslint-comments/no-unused-disable': 'off',
+      'i18n-text/no-en': 'off',
+      'import/no-commonjs': 'off',
+      'import/no-namespace': 'off',
+      'no-console': 'off',
+      'no-unused-vars': 'off',
+      'prettier/prettier': 'error',
+      semi: 'off'
+    }
+  }
+]
