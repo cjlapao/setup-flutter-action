f8a54652b2ed4fd6b76f23641dd954b2cf8cff73
Carlos Lapao <cjlapao@gmail.com>
2026-03-10 07:47:41 +0000

    wip

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-    entry: { index: 'src/main.js' },
-    format: ['cjs'],
-    platform: 'node',
-    outDir: 'dist',
-    splitting: false,
-    sourcemap: true,
-    bundle: true,
-    noExternal: [/.*/],
-    esbuildOptions(options) {
-        options.conditions = ['import', 'default']
-    }
+  entry: { index: 'src/main.js' },
+  format: ['cjs'],
+  platform: 'node',
+  outDir: 'dist',
+  splitting: false,
+  sourcemap: true,
+  bundle: true,
+  noExternal: [/.*/],
+  esbuildOptions(options) {
+    options.conditions = ['import', 'default']
+  }
 })
